b9574780ca600a29b88cd52b878603bd4690830d
AJ Rod <alpharodj@gmail.com>
2021-04-08 16:54:35 -0400

    Styled pages, fixed username not in SIGNUP form

--- carbon_dating/settings.py
+++ carbon_dating/settings.py
@@ -143,3 +143,9 @@ AUTH_USER_MODEL = 'author.Author'
 LOGIN_REDIRECT_URL = 'homepage'
 
 LOGOUT_REDIRECT_URL = 'homepage'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static"),
+]
