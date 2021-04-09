e5e976307423384d3d4e07cbc7a56741ee788826
AJ Rod <alpharodj@gmail.com>
2021-04-09 15:20:16 -0400

    Have a homepage interface, cannot view unless signed in, displaying ALL posts. (will be edited to view only admiring)

--- author/views.py
+++ author/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from post.models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def home_view(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()[:10]
+    context = {
+        "posts": posts
+    }
+    return render(request, 'home.html', context)
