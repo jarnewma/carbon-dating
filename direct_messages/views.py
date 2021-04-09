da00e53afb0beee3f80b4e31ea4e91ab050f9375
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2021-04-09 14:10:30 -0400

    Initial view setup for direct-messaging

--- direct_messages/views.py
+++ direct_messages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
+
+class MessagesView(LoginRequiredMixin, View):
+    def get(self, request, username):
+        pass
+
+    def post(self, request):
+        pass
