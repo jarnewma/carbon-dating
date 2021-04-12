20ae46962fb3f30573348478c7d48487ca4cc7ab
KAkevinlin <linkevin11911@gmail.com>
2021-04-12 03:34:41 -0500

    rendering user information, need to know what else we want on the page

--- author/views.py
+++ author/views.py
@@ -4,8 +4,9 @@ from author.models import Author
 
 
 def author_profile(request, user_id):
-    user_obj = Author.objeccts.get(id=user_id)
+    user_obj = Author.objects.get(id=user_id)
 
-    return render(request, "author_detail.html",{
+    return render(request, "author_detail.html", {
         "author_info": user_obj
     })
+
