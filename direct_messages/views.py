07dc2eb7eb3edfb7d6d3ee7540a141aee0e167bf
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2021-04-13 15:05:02 -0400

    messages_recieved and messages sent are now stored correctly

--- direct_messages/views.py
+++ direct_messages/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from author.models import Author
+from direct_messages.models import Message
 
 
 # Create your views here.
 
 class MessagesView(LoginRequiredMixin, View):
     def get(self, request, username):
+        matched_user = Author.objects.filter(username=username).first()
+        messages_recieved = Message.objects.filter(sender=matched_user, receiver=request.user)
+        messages_sent = Message.objects.filter(sender=request.user, receiver=matched_user)
         return render(request, "messages/direct_messages.html")
 
     def post(self, request):
