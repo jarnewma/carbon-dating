7a48d72f142911e28ef7b50bfbb737def004541a
Jason <jarnewma@gmail.com>
2021-04-15 09:46:52 -0400

    styling incomplete at the moment

--- comment/models.py
+++ comment/models.py
@@ -5,7 +5,7 @@ from post.models import Post
 
 
 class Comment(models.Model):
-    original_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    original_post = models.ForeignKey(Post, related_name="comment", on_delete=models.CASCADE)
     commenter = models.ForeignKey(Author, on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
     timestamp = models.DateTimeField(default=timezone.now)
