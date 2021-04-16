0642018f2f254e2b73f113cfe0fd48addd244b7c
KAkevinlin <linkevin11911@gmail.com>
2021-04-15 22:38:31 -0500

    password update form working

--- author/views.py
+++ author/views.py
@@ -3,6 +3,9 @@ from post.models import Post
 from author.models import Author
 from django.contrib.auth.decorators import login_required
 from authentication.forms import AuthorChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -83,8 +86,11 @@ def change_profile(request):
         'rock_type': request.user.rock_type,
         "interested_in": request.user.interested_in,
         "bio": request.user.bio,
-        "password":request.user.password
-        
+        'password': ""
     })
-    return render(request, "generic_form.html",{"form":form})
+    return render(request, "generic_form.html",{"form": form})
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('homepage')
     
--- carbon_dating/urls.py
+++ carbon_dating/urls.py
@@ -22,6 +22,8 @@ from author import views as author_views
 # from authentication.views import SignUpView
 from authentication import views as authentication_views
 from direct_messages.urls import url_patterns as messages_urls
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,6 +36,7 @@ urlpatterns = [
     path('admire/<int:user_id>/', author_views.admire_view),
     path('profile_admire/<int:user_id>/', author_views.profile_admire_view),
     path('change_profile/', author_views.change_profile, name="change_profile"),
+    path('password/', author_views.PasswordsChangeView.as_view(template_name="change_password.html")),
     *messages_urls,
 ]
 
